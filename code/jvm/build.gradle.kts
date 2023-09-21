13ba453b12cefaf8e9425fbdc6acc64832d963eb
RodrigoHCorreia <rodrigo.h.correia@gmail.com>
2023-09-21 13:22:07 +0100

    Added spring-web.

--- code/jvm/build.gradle.kts
+++ code/jvm/build.gradle.kts
@@ -21,6 +21,8 @@ repositories {
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.springframework:spring-web:6.0.12")
+
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 
     runtimeOnly("org.postgresql:postgresql")
--- code/jvm/src/main/kotlin/gomoku/server/http/controllers/HomeController.kt
+++ code/jvm/src/main/kotlin/gomoku/server/http/controllers/HomeController.kt
@@ -1,5 +1,7 @@
 package gomoku.server.http.controllers
 
+import org.springframework.web.bind.annotation.RestController
+
 @RestController
 class HomeController {
 }
