71554969c505f92342c5ccdc400c13d8d2c9b7cf
Sideghost <guilherme.cordeiro.pereira@gmail.com>
2023-09-27 08:21:03 +0100

    After RFC 2.0

--- code/sql/createTable.sql
+++ code/sql/createTable.sql
@@ -1,57 +1,62 @@
-//database model
-CREATE TABLE IF NOT EXISTS 'user' (
-    id int NOT NULL PRIMARY KEY,
-    username varchar(255) NOT NULL,
+create table if not exists 'user' (
+    uuid serial primary key,
+    username varchar(255) not null,
 
     unique(username)
 );
 
+create table if not exists 'password' (
+    user_id int not null primary key,
+    encoded_password varchar(255) not null,
+    method varchar(255) not null,
 
-CREATE TABLE IF NOT EXISTS 'password' (
-    user_id int NOT NULL PRIMARY KEY REFERENCES 'user'(uuid),
-    encoded_password varchar(255) NOT NULL,
-    method varchar(255) NOT NULL
+    constraint fk_user foreign key(user_id) references 'user'(uuid),
 );
 
+create table if not exists 'token' (
+    user_id int not null primary key,
+    encoded_token varchar(255) not null,
+    create_date timestamp not null,
+    last_used timestamp not null,
+    ttl time not null,
 
-CREATE TABLE IF NOT EXISTS 'token' (
-    user_id int NOT NULL PRIMARY KEY REFERENCES 'user'(uuid),
-    encoded_token varchar(255) NOT NULL,
-    create_date timestamp NOT NULL,
-    last_used timestamp NOT NULL,
-    ttl time NOT NULL,
-
-    CONSTRAINT positive_ttl CHECK (ttl > 0),
-    CHECK (last_used > create_date),
-    CHECK (create_date + ttl > last_used)
+    constraint fk_user foreign key(user_id) references 'user'(uuid),
+    constraint positive_ttl check (ttl > 0),
+    check (last_used > create_date),
+    check (create_date + ttl > last_used)
 );
 
+create table if not exists 'stats' (
+    user_id int not null primary key,
+    elo int not null default 0,
+    games_played int not null default 0,
 
-CREATE TABLE IF NOT EXISTS 'stats' (
-    user_id int NOT NULL PRIMARY KEY REFERENCES 'user'(uuid)
-    elo int NOT NULL DEFAULT 0,
-    games_played int NOT NULL DEFAULT 0
+    constraint fk_user foreign key(user_id) references 'user'(uuid)
 );
 
+create table if not exists 'match' (
+    match_id int not null primary key,
+    host_id int not null,
 
-CREATE TABLE IF NOT EXISTS 'match' (
-    match_id int NOT NULL PRIMARY KEY,
-    host_id int NOT NULL REFERENCES 'user'(uuid),
+    constraint fk_user foreign key(host_id) references 'user'(uuid)
 );
 
+create table if not exists 'ongoing_match' (
+    match_id int not null primary key,
+    match_host_id int not null,
+    guest_id int not null,
+    moves array,
 
-CREATE TABLE IF NOT EXISTS 'ongoing_match' (
-    match_id int NOT NULL PRIMARY KEY REFERENCES 'match'(match_id),
-    match_host_id int NOT NULL REFERENCES 'match'(host_id),
-    guest_id int REFERENCES 'user'(uuid),
-    moves ARRAY,
-
-    CONSTRAINT host_different_than_guess CHECK(host_id <> guest_id)
+    constraint fk_match foreign key(match_id) references 'match'(match_id),
+    constraint fk_match foreign key(match_host_id) references 'match'(host_id),
+    constraint fk_user foreign key(guest_id) references 'user'(uuid),
+    constraint host_different_than_guess check (host_id <> guest_id)
 );
 
+create table if not exists 'finished_match' (
+    match_id int not null primary key,
+    moves array not null,
+    winner int not null,
 
-CREATE TABLE IF NOT EXISTS 'finished_match' (
-    match_id int NOT NULL PRIMARY KEY REFERENCES 'match'(match_id),
-    moves ARRAY NOT NULL,
-    winner int NOT NULL
+    constraint fk_ongoing_match foreign key(match_id) references 'ongoing_match'(match_id)
 );
