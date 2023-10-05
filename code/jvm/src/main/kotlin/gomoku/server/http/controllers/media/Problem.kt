838dfb0600dd773d415a3006fbdec4eff520156d
Rodrigohcorreia <rodrigo.h.correia@gmail.com>
2023-10-05 13:13:03 +0100

    Added URI to type when sending response with Problem

--- code/jvm/src/main/kotlin/gomoku/server/http/controllers/media/Problem.kt
+++ code/jvm/src/main/kotlin/gomoku/server/http/controllers/media/Problem.kt
@@ -10,6 +10,7 @@ class Problem(
 
     companion object {
         const val MEDIA_TYPE = "application/problem+json"
+        private const val PROBLEM_BASE_URL = "https://github.com/isel-leic-daw/2023-daw-leic51d-01/main/docs/problems/"
         fun response(status: Int, problem: Problem) = ResponseEntity
             .status(status)
             .header("Content-Type", MEDIA_TYPE)
@@ -17,34 +18,34 @@ class Problem(
 
         val userAlreadyExists = Problem(
             URI(
-                "TODO:"
+                PROBLEM_BASE_URL + "user-already-exists"
             )
         )
         val insecurePassword = Problem(
             URI(
-                "TODO:"
+                PROBLEM_BASE_URL + "insecure-password"
             )
         )
         val invalidUsername = Problem(
             URI(
-                "TODO:"
+                PROBLEM_BASE_URL + "invalid-username"
             )
         )
         val userOrPasswordAreInvalid = Problem(
             URI(
-                "TODO:"
+                PROBLEM_BASE_URL + "user-or-password-are-invalid"
             )
         )
 
         val invalidRequestContent = Problem(
             URI(
-                "TODO:"
+                PROBLEM_BASE_URL + "invalid-request-content"
             )
         )
 
         val userNotFound = Problem(
             URI(
-                "TODO:"
+                PROBLEM_BASE_URL + "user-not-found"
             )
         )
     }
