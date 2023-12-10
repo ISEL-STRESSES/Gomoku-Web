44be2546b835eccb927dee1bb2cf7b5d0cef6649
48335 - Rodrigo Correia <a48335@alunos.isel.pt>
2023-12-10 15:38:04 +0000

    Board is no longer invisible!

--- code/js/src/components/game/shared/BoardView.tsx
+++ code/js/src/components/game/shared/BoardView.tsx
@@ -1,73 +1,82 @@
-import Grid from "@mui/material/Grid"
-import * as React from "react"
-import {ForwardedRef} from "react"
-import Box from "@mui/material/Box"
-import Tile, {tileSize} from "./Tile"
+import { ForwardedRef } from "react";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
 import { MovesOutputModel } from "../../../service/game/models/GameOutput";
+import boardCenter from '../../../assets/board-center.png';
+import boardCorner from '../../../assets/board-corner.png';
+import boardLateral from '../../../assets/board-latteral.png';
+import { tileSize } from "./Tile";
+import * as React from 'react';
 
-/**
- * Properties for the BoardView component.
- *
- * @property board the board to display
- * @property enabled whether the board is enabled or not
- * @property onTileClicked the callback to be called when a tile is clicked
- * @property children the children to be displayed on top of the board
- */
 interface BoardViewProps {
-    board: MovesOutputModel
-    enabled: boolean
-    onTileClicked?: (x:number, y:number) => void
-    children?: React.ReactNode
+  board: MovesOutputModel;
+  enabled: boolean;
+  onTileClicked?: (x: number, y: number) => void;
+  children?: React.ReactNode;
 }
 
-/**
- * BoardView component.
- */
-function BoardView({board, enabled, onTileClicked, children}: BoardViewProps, ref: ForwardedRef<HTMLDivElement>) {
-    return (
-        <Box
-            sx={{
-                width: board.boardSize * tileSize,
-                height: board.boardSize * tileSize,
-                margin: 'auto',
-                position: "relative",
-                opacity: enabled ? 1 : 0.5
-            }}>
-            <Grid ref={ref} container columns={board.boardSize}>
-                {
-                    Array.from(Array(board.boardSize * board.boardSize).keys()).map((tileIndex) => {
-                            const col = tileIndex % board.boardSize
-                            const row = Math.floor(tileIndex / board.boardSize)
+function BoardView({ board, enabled, onTileClicked, children }: BoardViewProps, ref: ForwardedRef<HTMLDivElement>) {
+  const getTileStyle = (col: number, row: number, boardSize: number) => {
+    let backgroundImage: string;
+    let transform = '';
 
-                            return <Grid item key={tileIndex} xs={1} sm={1} md={1}>
-                                {
-                                    tileIndex == 0
-                                        ? <Box sx={{width: tileSize, height: tileSize}}/>
-                                        : (
-                                            tileIndex <= board.boardSize - 1
-                                                ? <Box sx={{width: tileSize, height: tileSize}}>
-                                                    {String.fromCharCode(64 + tileIndex)}
-                                                </Box>
-                                                : (
-                                                    col === 0
-                                                        ? <Box sx={{width: tileSize, height: tileSize}}>
-                                                            {row}
-                                                        </Box>
-                                                        : <Tile onClick={() => {
-                                                            if (onTileClicked && enabled)
-                                                                onTileClicked(col, row)
-                                                        }}/>
-                                                )
-                                        )
-                                }
-                            </Grid>
-                        }
-                    )
-                }
+    const isCorner = (col === 0 || col === boardSize - 1) && (row === 0 || row === boardSize - 1);
+    const isLateral = col === 0 || col === boardSize - 1 || row === 0 || row === boardSize - 1;
+
+    if (isCorner) {
+      backgroundImage = `url(${boardCorner})`;
+      if (col === boardSize - 1 && row === 0) transform = 'rotate(90deg)';
+      if (col === boardSize - 1 && row === boardSize - 1) transform = 'rotate(180deg)';
+      if (col === 0 && row === boardSize - 1) transform = 'rotate(270deg)';
+    } else if (isLateral) {
+      backgroundImage = `url(${boardLateral})`;
+      if (row === 0) transform = 'rotate(90deg)';
+      if (col === boardSize - 1) transform = 'rotate(180deg)';
+      if (row === boardSize - 1) transform = 'rotate(270deg)';
+    } else {
+      backgroundImage = `url(${boardCenter})`;
+    }
+
+    return {
+      width: tileSize,
+      height: tileSize,
+      backgroundImage: backgroundImage,
+      transform: transform,
+      backgroundSize: 'contain',
+      backgroundRepeat: 'no-repeat',
+    };
+  };
+
+  return (
+    <Box
+      sx={{
+        width: board.boardSize * tileSize,
+        height: board.boardSize * tileSize,
+        margin: 'auto',
+        position: "relative",
+        opacity: enabled ? 1 : 0.5
+      }}
+      ref={ref}
+    >
+      <Grid container columns={board.boardSize}>
+        {Array.from(Array(board.boardSize * board.boardSize).keys()).map((tileIndex) => {
+          const col = tileIndex % board.boardSize;
+          const row = Math.floor(tileIndex / board.boardSize);
+          const tileStyle = getTileStyle(col, row, board.boardSize);
+
+          return (
+            <Grid item key={tileIndex} xs={1} sm={1} md={1}>
+              <Box
+                sx={tileStyle}
+                onClick={() => onTileClicked && enabled && onTileClicked(col, row)}
+              />
             </Grid>
-            {children}
-        </Box>
-    )
+          );
+        })}
+      </Grid>
+      {children}
+    </Box>
+  );
 }
 
-export default React.forwardRef(BoardView)
+export default React.forwardRef(BoardView);
