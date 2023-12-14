c17473bbc93c7f548bec96e3a7f98fdc83afbfec
Sideghost <guilherme.cordeiro.pereira@gmail.com>
2023-12-14 11:55:46 +0000

    fixes to deployment

--- code/js/webpack.config.js
+++ code/js/webpack.config.js
@@ -1,4 +1,5 @@
 const ESLintPlugin = require('eslint-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { resolve } = require('path');
 module.exports = {
   mode: 'development',
@@ -12,6 +13,10 @@ module.exports = {
   plugins: [
     new ESLintPlugin({
       extensions: ['js', 'jsx', 'ts', 'tsx'],
+    }),
+    new HtmlWebpackPlugin({
+      template: './public/index.html', // path to your source index.html
+      favicon: './public/favicon.ico',
     }),
   ],
   module: {
